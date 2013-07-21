906e1f3e2fe49d80bccb7b8b971bfc09e118b3d3
Dijon Kock <dijon93@hotmail.com>
2013-07-21 12:48:22 +0000

    try 1 to modify web.js to read & outpute file index.html

--- web.js
+++ web.js
@@ -2,8 +2,11 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+//read data from a file, copy it into a buffer, convert that into a string,
+var index = fs.readFileSync(index.html);//read data from a file
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(index.toString(););
 });
 
 var port = process.env.PORT || 5000;
